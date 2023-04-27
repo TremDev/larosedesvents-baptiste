42a9a7ecb54f4d652cdc631de7f78f9128e05e63
TremDev <kevin1.tremerel@epitech.eu>
2023-04-27 22:03:01 +0200

    Add links array && browserRouter && routerProvider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Contact, Galerie, Home, Showroom } from "./Pages";
+import { createBrowserRouter, redirect, RouterProvider } from "react-router-dom";
 import App from "./App";
+
+const links = [
+  {
+    pageName: "home",
+    title: "Home",
+    renderPage: <Home />,
+  },
+  {
+    pageName: "showroom",
+    title: "Showroom",
+    renderPage: <Showroom />,
+  },
+  {
+    pageName: "galerie",
+    title: "Galerie",
+    renderPage: <Galerie />,
+  },
+  {
+    pageName: "contact",
+    title: "Contact",
+    renderPage: <Contact />,
+  },
+];
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    loader: () => redirect('/home'),
+  },
+  {
+    path: "/:pageName",
+    element: (
+      <App links={links.map((o) => ({ ...o, render: o?.renderPage }))} />
+    ),
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
