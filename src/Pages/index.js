dbac90322065c6cbf8446c155d0665762d65e877
TremDev <kevin1.tremerel@epitech.eu>
2023-04-27 20:40:28 +0200

    Add all pages

---
+++ src/Pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home/Home'
+export { default as Showroom } from './Showroom/Showroom'
+export { default as Galerie } from './Galerie/Galerie'
+export { default as Contact } from './Contact/Contact'
