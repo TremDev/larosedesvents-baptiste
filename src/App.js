2187fd92e508fbdda5666d85b27cebac8b95b7ad
TremDev <kevin1.tremerel@epitech.eu>
2023-04-27 22:05:51 +0200

    add header and logic for rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { useParams } from "react-router-dom";
+import { Header } from "./Layout";
 
-const App = () => {
-  return <div></div>;
-}
+const App = (props) => {
+  const params = useParams();
+  const { links = [], paramKey = "pageName" } = props;
+
+  return (
+    <>
+      <Header
+        tabs={links?.map((value) => ({
+          title: value?.title,
+          active: value?.pageName === params?.[paramKey],
+          href: `/${value?.pageName}`,
+          icon: value?.icon,
+        }))}
+      />
+      {links?.find((o) => o?.pageName === params?.[paramKey])?.render}
+    </>
+  );
+};
 
 export default App;
